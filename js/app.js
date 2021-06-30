e983574935912264547a2a8d4adef5397c80c22a
KarinaPortuguez <karinaportuguezh@gmail.com>
2021-06-30 11:20:16 -0500

    Se agruparon los archivos js en una carpeta y se agrego el archivo index

--- js/app.js
+++ js/app.js
@@ -3,12 +3,15 @@
 // Son los planos o bases para crear un objeto multiples veces
 // INVESTIGAR LOS PROTOTIPES
 
+//Las clases inician con mayuscula
+
 class Tarea {
-  constructor(nombre, prioridad){
+  constructor(nombre, prioridad){  //El constructor pasa los argumentos hacia el objeto y se ejecuta primero al crear una instancia
     this.nombre = nombre;
     this.prioridad = prioridad;
   }
-  mostrar(){
+  //cuando pasas una funcion a una clase es un metodo
+  mostrar(){ // metodo o prototype o funcion dentro de una clase
     return `La tarea: ${this.nombre}, tiene una prioridad: ${this.prioridad}`;
   }
 }
@@ -24,3 +27,7 @@ console.log(tarea1.mostrar());
 console.log(tarea2.mostrar());
 console.log(tarea3.mostrar());
 console.log(tarea4.mostrar());
+
+//Las classes solo se corren en verciones modernas de javascript
+// si se prueba esto en internet explorer 8 o 9 no lo soporta
+//pero hay formas de hacerlo compatible
